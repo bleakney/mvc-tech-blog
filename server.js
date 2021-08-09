33f0a735a5debb8d4161b645291f43c7582ca2b7
Bleakney Bissett <bleakneybissett@gmail.com>
2021-08-09 13:19:18 -0500

    set up server file and dependencies

---
+++ server.js
@@ -0,0 +1,42 @@
+// set up dependencies
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize');
+
+// set up session persistence
+const sess = {
+    secret: "grandma's chocolate chip cookies",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(session(sess));
+
+// express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set up handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening!'));
+});
