c68d13603d9ee8821aa6a8d094e3af8c785cd117
Bleakney Bissett <bleakneybissett@gmail.com>
2021-08-11 15:54:48 -0500

    set up login page ui and homepage ui, send posts to homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
+router.get('/login', (req, res) => {
+  res.render('login');
+})
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  // console.log(req.session);
+  Post.findAll({
+    attributes: ["id", "title", "content", "user_id", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id'],
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(postData => {
+    const posts = postData.map(post => post.get({ plain: true }));
+    res.render('homepage', { posts });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
+
+
 
 module.exports = router;
