47c28edcb715631f421b5176ed163113b50609bc
Bleakney Bissett <bleakneybissett@gmail.com>
2021-08-11 11:15:35 -0500

    create User routes and setup index.js file to pass all api routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,1 +1,123 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+
+// get all user info
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((userData) => res.json(userData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// get one unique user's info
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ["id", "title", "created_at"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "created_at"],
+        include: {
+          model: Post,
+          attributes: ["title"],
+        },
+      },
+    ],
+  })
+    .then((userData) => {
+      if (!userData) {
+        res.status(404).json({ message: "No user found with this id!" });
+        return;
+      }
+      res.json(userData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  // expects {"username": "", "email": "", "password": ""}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    // .then(userData => {
+    //     req.session.save(() => {
+    //         req.session.user_id = userData.id;
+    //         req.session.username = userData.username;
+    //         req.session.loggedIn = true;
+
+    //         res.json(userData)
+    //     })
+    // })
+    .then((userData) => {
+      res.json(userData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// LOGIN POST REQ GOES HERE
+
+// LOGOUT POST REQ GOES HERE
+
+// update one user's data by id
+router.put('/:id', (req, res) => {
+    // expects {"username": "", "email": "", "password": ""}
+    
+    // pass in req.body to only update what's passed through the put request
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(userData => {
+        if(!userData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.json(userData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(userData => {
+        if(!userData) {
+            res.status(404).json({ message: "No user found with this id!" });
+            return;
+        }
+        res.status(200).json({ message: "Successfully deleted!" });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -2,7 +2,6 @@
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
-const { post } = require('../controllers');
 
 // create associations
 User.hasMany(Post, {
