a141ba670f7206f63d4bc88553a603fef51d612f
Bleakney Bissett <bleakneybissett@gmail.com>
2021-08-13 15:34:09 -0500

    add logout functionality and routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,8 +8,51 @@ router.get('/login', (req, res) => {
   res.render('login');
 })
 
+router.get('/logout', (req, res) => {
+    res.render('login');
+});
+
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ["id", "title", "content", "user_id", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["id", "username"]
+        }
+      },
+      {
+        model: User,
+        attributes: ["id", "username"]
+      }
+    ]
+  })
+  .then(postData => {
+    if(!postData) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    };
+    // serialize the data
+    const post = postData.get({ plain: true });
+    console.log(post);
+
+    // pass data to template 
+    res.render('single-post', { post })
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
 router.get('/', (req, res) => {
-  // console.log(req.session);
+  console.log(req.session);
   Post.findAll({
     attributes: ["id", "title", "content", "user_id", "created_at"],
     include: [
