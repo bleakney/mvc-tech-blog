01dfdeec9db8791a106be0ce51779957cdee547d
Bleakney Bissett <bleakneybissett@gmail.com>
2021-08-14 18:44:08 -0500

    set up conditional rendering for commenting and login/logout buttons

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,42 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 
+// GET LOGIN PAGE
 router.get('/login', (req, res) => {
   if(req.session.loggedIn) {
     res.redirect('/');
+    return;
   }
   res.render('login');
-})
+});
 
-router.get('/logout', (req, res) => {
-    res.render('login');
+// GET HOMEPAGE
+router.get('/', (req, res) => {
+  console.log(req.session);
+  Post.findAll({
+    attributes: ["id", "title", "content", "user_id", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id'],
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(postData => {
+    const posts = postData.map(post => post.get({ plain: true }));
+    res.render('homepage', { 
+      posts,
+      loggedIn: req.session.loggedIn
+     });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
 
 router.get('/post/:id', (req, res) => {
@@ -43,7 +70,9 @@ router.get('/post/:id', (req, res) => {
     console.log(post);
 
     // pass data to template 
-    res.render('single-post', { post })
+    res.render('single-post', { 
+      post,
+    loggedIn: req.session.loggedIn });
   })
   .catch(err => {
     console.log(err);
@@ -51,30 +80,7 @@ router.get('/post/:id', (req, res) => {
   });
 });
 
-router.get('/', (req, res) => {
-  console.log(req.session);
-  Post.findAll({
-    attributes: ["id", "title", "content", "user_id", "created_at"],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id'],
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-  .then(postData => {
-    const posts = postData.map(post => post.get({ plain: true }));
-    res.render('homepage', { posts });
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err)
-  });
-});
+
 
 
 
