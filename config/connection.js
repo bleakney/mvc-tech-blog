9e726f8b9e1b8d96a015d66634e98d5bb74f43c5
Bleakney Bissett <bleakneybissett@gmail.com>
2021-08-14 21:36:16 -0500

    fix seeds?

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,20 @@
-const Sequelize = require('sequelize');
 const seedAll = require('../seeds/index');
+const Sequelize = require('sequelize');
 
 require('dotenv').config()
 
-// create connection to our database, pass in mysql credentials
-let sequelize;
-
-
+// seed database
+seedAll();
 
 // use credentials through jawsdb if accessing heroku deployment
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-     // seed database
-    seedAll();
-} else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    });
-};
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    })
 
 module.exports = sequelize;
