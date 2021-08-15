59963803379c2b3c760c1f31f1acecb174487d7b
Bleakney Bissett <bleakneybissett@gmail.com>
2021-08-14 21:21:06 -0500

    seed database

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,12 @@ require('dotenv').config()
 // create connection to our database, pass in mysql credentials
 let sequelize;
 
-// seed database
-seedAll();
 
 // use credentials through jawsdb if accessing heroku deployment
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
+    // seed database
+    seedAll();
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
