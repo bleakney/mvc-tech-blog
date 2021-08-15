6f68fd2d684e54c4d13d52e700b1b07904ed38b2
Bleakney Bissett <bleakneybissett@gmail.com>
2021-08-14 21:09:01 -0500

    edit and delete posts from user dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -34,4 +34,44 @@ router.get('/', withAuth, (req, res) => {
         });
       });
 
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+          id: req.params.id
+        },
+        attributes: ["id", "title", "content", "user_id", "created_at"],
+        include: [
+          {
+            model: Comment,
+            attributes: ["id", "comment_text", "user_id", "created_at"],
+            include: {
+              model: User,
+              attributes: ["id", "username"]
+            }
+          },
+          {
+            model: User,
+            attributes: ["id", "username"]
+          }
+        ]
+      })
+      .then(postData => {
+        if(!postData) {
+          res.status(404).json({ message: "No post found with this id!" });
+          return;
+        };
+        // serialize the data
+        const post = postData.get({ plain: true });
+        console.log(post);
+    
+        // pass data to template 
+        res.render('edit-post', { 
+          post,
+        loggedIn: req.session.loggedIn });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+    });
 module.exports = router;
